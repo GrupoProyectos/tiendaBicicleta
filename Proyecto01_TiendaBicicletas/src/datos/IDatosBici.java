628bc239195c3e1988c85213f873fc2ce747c69b
Iván <dagozoth@gmail.com>
2018-06-28 14:26:58 +0200

    Auto stash before merge of "local" and "origin/local"
    Creo interfaz e implementación de datos

---
+++ Proyecto01_TiendaBicicletas/src/datos/IDatosBici.java
@@ -0,0 +1,14 @@
+package datos;
+
+import java.util.List;
+
+import modelo.Bicicleta;
+
+public interface IDatosBici {
+	
+	public List<Bicicleta> mostrarCatalogo();
+	
+	
+	
+
+}
---
+++ Proyecto01_TiendaBicicletas/src/datos/implementado/DatosBiciImp.java
@@ -0,0 +1,18 @@
+package datos.implementado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import datos.IDatosBici;
+import modelo.Bicicleta;
+
+public class DatosBiciImp implements IDatosBici {
+
+	public List<Bicicleta> mostrarCatalogo() {
+
+		List<Bicicleta> catalogo = new ArrayList<>();
+
+		return catalogo;
+	}
+
+}
