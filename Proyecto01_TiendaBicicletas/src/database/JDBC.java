1c46399a10ed22a90ed31422ce678e613cbc3020
Alejandro <alejandrodelcastillonovella@gmail.com>
2018-06-28 15:15:36 +0200

    JDBC CREADO
    Conexión a la BBDD creada.
    Método "conexion()" para conectarnos a la BBDD.
    Método "peticionCatalogo()" para lanzar la SELECT.

---
+++ Proyecto01_TiendaBicicletas/src/database/JDBC.java
@@ -0,0 +1,45 @@
+package database;
+
+import java.sql.*;
+import javax.sql.*;
+
+public class JDBC {
+
+	static Connection con;
+	static Statement st;
+	static ResultSet rs;
+
+	public JDBC() {
+
+	}
+
+	public void conexion() throws SQLException {
+		try {
+			String driverClassName = "com.mysql.jdbc.Driver"; // comprobamos la  existencia del driver JDBC 
+			Class.forName(driverClassName); 
+
+			String driverUrl = "jdbc:mysql://10.90.36.31/tiendabicis"; // enlace hacia la BBDD
+
+			String user = "root"; // usuario de la BBDD
+			String password = "1111"; // contraseña del usuario
+
+			con = DriverManager.getConnection(driverUrl, user, password); // generamos la conexion
+			
+		} catch (ClassNotFoundException e) { // Excepción del driver
+			System.out.println("No se encuentra el driver");
+		} /*
+			 * catch (SQLException e) { System.out.println("Excepcion SQL: " +
+			 * e.getMessage()); System.out.println("Estado SQL: " +
+			 * e.getSQLState()); System.out.println("Código del Error: " +
+			 * e.getErrorCode()); }
+			 */
+	}
+
+	public void peticionCatalogo() throws SQLException {
+		st = con.createStatement(); // creamos un objeto de la Clase Statement
+
+		String query = "SELECT * FROM bicicleta"; // lanzamos la petición
+		rs = st.executeQuery(query); //
+	}
+
+}
