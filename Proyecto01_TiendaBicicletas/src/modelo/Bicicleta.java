48b6dffea077513273af25d1e8c4f181117f7ed1
Chen <chen90jj@gmail.com>
2018-06-28 14:18:22 +0200

    Mejorar el metodo toString y corregir el constructor

--- Proyecto01_TiendaBicicletas/src/modelo/Bicicleta.java
+++ Proyecto01_TiendaBicicletas/src/modelo/Bicicleta.java
@@ -1,19 +1,20 @@
 package modelo;
 
 public class Bicicleta {
-	
+
 	private int idBicicleta;
 	private Categoria categoria;
 	private String marca;
 	private String modelo;
 	private Talla talla;
 	private double precio;
-	
-	public Bicicleta(){
-		
+
+	public Bicicleta() {
+
 	}
-	
-	public void Bicicleta(int idBicicleta, Categoria categoria, String marca, String modelo, Talla tamano, double precio){
+
+	public Bicicleta(int idBicicleta, Categoria categoria, String marca, String modelo, Talla talla, double precio) {
+		super();
 		this.idBicicleta = idBicicleta;
 		this.categoria = categoria;
 		this.marca = marca;
@@ -21,12 +22,15 @@ public class Bicicleta {
 		this.talla = talla;
 		this.precio = precio;
 	}
-	public int getIdBicicleta(){
+
+	public int getIdBicicleta() {
 		return idBicicleta;
 	}
-	public void setIdBicicleta(int idBicicleta){
+
+	public void setIdBicicleta(int idBicicleta) {
 		this.idBicicleta = idBicicleta;
 	}
+
 	public Categoria getCategoria() {
 		return categoria;
 	}
@@ -69,12 +73,21 @@ public class Bicicleta {
 
 	@Override
 	public String toString() {
-		return "Bicicleta:"
-				+ "\n\t categoria -> " + categoria 
-				+ "\n\t marca -> " + marca 
-				+ "\n\t modelo -> " + modelo 
-				+ "\n\t talla -> " + talla
-				+ "\n\t precio -> " + precio;
+		StringBuilder builder = new StringBuilder();
+		builder.append("Bicicleta [idBicicleta=");
+		builder.append(idBicicleta);
+		builder.append(", categoria=");
+		builder.append(categoria);
+		builder.append(", marca=");
+		builder.append(marca);
+		builder.append(", modelo=");
+		builder.append(modelo);
+		builder.append(", talla=");
+		builder.append(talla);
+		builder.append(", precio=");
+		builder.append(precio);
+		builder.append("]");
+		return builder.toString();
 	}
-	
+
 }
